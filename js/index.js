221527e9f64f28409c68c6e8d116f9e01f90ab8e
lxj9 <347344217@qq.com>
2019-05-16 10:51:11 +0800

    提交

---
+++ js/index.js
@@ -0,0 +1,32 @@
+/*      图片透明度动画   */
+
+
+
+
+/*function startMove(target){
+			  	var heart=document.getElementById('heart');
+
+				var alp=heart.getElementsByTagName('div');
+
+				clearInterval(timer);
+
+				timer=setInterval(function(){
+					var speed=0;
+					if(alpha<target){
+						speed=10;
+					}else{
+						speed=-10;
+					}
+
+					if(alpha==target){
+						clearInterval(timer);
+					}else{
+						alpha+=speed;
+
+						alp.style.filter='alpha(opacity:+"alpha"+)';
+						alp.style.opacity=alpha/100;
+					}
+				},30)
+			}
+
+			*/
